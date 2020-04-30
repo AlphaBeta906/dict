6676addd7c989246e0b6828cec7dbb31db8bac7d
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2020-04-30 19:51:23 +0800

    Add files via upload

---
+++ conlang dict..py
@@ -0,0 +1,19 @@
+ 
+import time as t
+print ("Altanio Conlang Dictionary Ver. 1 Alpha 1")
+t.sleep(2)
+a = {"people": "Alta", "land": "Garda", "sea": "Souhl", "city": "Altagarda", "crop": "Wazha",}
+words = ["people", "land", "sea", "city", 'crop']
+while True:
+    question = input("Word? P.S: Do not make it any character upper")
+    #NOTE TO SELF: FIX BIG UPPER OR LOWER
+    t.sleep(2)
+    if question in words:
+        question = question.lower()
+        print (a[question] + " is " + question)
+    elif question == "end" or "END":
+        print ("Ended")
+        break
+    else:
+        print ("Not in dict")
+#LOOP GOOD :D
