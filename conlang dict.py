5768ba077d0586765029da8613ca5b7f49fc782a
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2020-05-08 14:22:23 +0800

    Update conlang dict.py

--- conlang dict.py
+++ conlang dict.py
@@ -1,26 +1,26 @@
-#IF JOINING PLS COMMIT NEWEST ONE!!! 
-import time as t
-print ("Altanio Conlang Dictionary Ver. 1 Alpha 1")
-import socket as s
-name = s.gethostbyname()
-IP = s.gethostname(name)
-if IP = 192.168.5.101:
-    print ("Hello Master Alpha")
-else:
-    print ("Hello Guest")
-t.sleep(2)
-a = {"people": "Alta", "land": "Garda", "sea": "Souhl", "city": "Altagarda", "crop": "Wazha",}
-words = ["people", "land", "sea", "city", 'crop']
-while True:
-    question = input("Word? P.S: Do not make it any character upper")
-    #NOTE TO SELF: FIX BIG UPPER OR LOWER
-    t.sleep(2)
-    if question in words:
-        question = question.lower()
-        print (a[question] + " is " + question)
-    elif question == "E":
-        print ("Ended")
-        break
+#IF JOINING PLS COMMIT NEWEST ONE!!!
+def hello():
+    print ("Altanio Conlang Dictionary Ver. 1 Alpha 1")
+    import socket as s
+    name = s.gethostbyname()
+    IP = s.gethostname(name)
+    if IP = 192.168.5.101:
+        print ("Hello Master Alpha")
     else:
-        print ("Not in dict")
+        print ("Hello Guest")
+def trans():
+    a = {"people": "Alta", "land": "Garda", "sea": "Souhl", "city": "Altagarda", "crop": "Wazha",}
+    words = ["people", "land", "sea", "city", 'crop']
+    while True:
+        question = input("Word? P.S: Do not make it any character upper")
+        #NOTE TO SELF: FIX BIG UPPER OR LOWER
+        t.sleep(2)
+        if question in words:
+            question = question.lower()
+            print (a[question] + " is " + question)
+        elif question == "E":
+            print ("Ended")
+            break
+        else:
+            print ("Not in dict")
 #LOOP GOOD :D
